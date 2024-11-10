fe97959824c0b3ed7f209f7b457654db0175d9ae
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2024-11-10 19:49:18 +0000

    model

--- src/app.py
+++ src/app.py
@@ -1,4 +1,83 @@
 from utils import db_connect
 engine = db_connect()
 
-# your code here
+import pandas as pd
+
+total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/k-means-project-tutorial/main/housing.csv")
+
+X = total_data[["MedInc", "Latitude", "Longitude"]]
+X.head()
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test = train_test_split(X, test_size = 0.2, random_state = 42)
+
+X_train.head()
+
+from sklearn.cluster import KMeans
+
+model_unsup = KMeans(n_clusters = 6, n_init = "auto", random_state = 42)
+model_unsup.fit(X_train)
+
+y_train = list(model_unsup.labels_)
+X_train["cluster"] = y_train
+X_train.head()
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+fig, axis = plt.subplots(1, 3, figsize = (15, 5))
+
+sns.scatterplot(ax = axis[0], data = X_train, x = "Latitude", y = "Longitude", hue = "cluster", palette = "deep")
+sns.scatterplot(ax = axis[1], data = X_train, x = "Latitude", y = "MedInc", hue = "cluster", palette = "deep")
+sns.scatterplot(ax = axis[2], data = X_train, x = "Longitude", y = "MedInc", hue = "cluster", palette = "deep")
+plt.tight_layout()
+
+plt.show()
+
+y_test = list(model_unsup.predict(X_test))
+X_test["cluster"] = y_test
+X_test.head()
+
+fig, axis = plt.subplots(1, 3, figsize = (15, 5))
+
+sns.scatterplot(ax = axis[0], data = X_train, x = "Latitude", y = "Longitude", hue = "cluster", palette = "deep", alpha  = 0.1)
+sns.scatterplot(ax = axis[1], data = X_train, x = "Latitude", y = "MedInc", hue = "cluster", palette = "deep", alpha  = 0.1)
+sns.scatterplot(ax = axis[2], data = X_train, x = "Longitude", y = "MedInc", hue = "cluster", palette = "deep", alpha  = 0.1)
+
+sns.scatterplot(ax = axis[0], data = X_test, x = "Latitude", y = "Longitude", hue = "cluster", palette = "deep", marker = "+")
+sns.scatterplot(ax = axis[1], data = X_test, x = "Latitude", y = "MedInc", hue = "cluster", palette = "deep", marker = "+")
+sns.scatterplot(ax = axis[2], data = X_test, x = "Longitude", y = "MedInc", hue = "cluster", palette = "deep", marker = "+")
+
+plt.tight_layout()
+
+for ax in axis:
+    ax.legend([],[], frameon=False)
+
+plt.show()
+
+from sklearn.tree import DecisionTreeClassifier
+
+model_sup = DecisionTreeClassifier(random_state = 42)
+model_sup.fit(X_train, y_train)
+
+from sklearn import tree
+
+fig = plt.figure(figsize=(15,15))
+
+tree.plot_tree(model_sup, feature_names = list(X_train.columns), class_names = ["0", "1", "2", "3", "4", "5"], filled = True)
+
+plt.show()
+
+y_pred = model_sup.predict(X_test)
+y_pred
+
+from sklearn.metrics import accuracy_score
+
+accuracy_score(y_test, y_pred)
+
+from pickle import dump
+
+dump(model_unsup, open("/workspaces/Kmeans-Lex-Brunett/models/k-means_default_42.sav", "wb"))
+dump(model_sup, open("/workspaces/Kmeans-Lex-Brunett/models/decision_tree_classifier_default_42.sav", "wb"))
+
